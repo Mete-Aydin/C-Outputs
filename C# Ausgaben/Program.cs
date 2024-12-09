7801d3a7534553608159d43109fd4c66a489a7d8
Mete-Aydin <mete.aydin180395@gmail.com>
2024-12-09 13:59:43 +0100

    Projektdateien hinzufügen.

---
+++ C# Ausgaben/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Ausgaben
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It a good Practice");
+            Console.WriteLine("I Love it!");
+            Console.WriteLine("It is awesome!");
+
+            //add
+            Console.WriteLine(3+3);
+            //subtract
+            Console.WriteLine(3-3);
+            //multiple
+            Console.WriteLine(3*3);
+            //divide
+            Console.WriteLine(10/2);
+
+           
+            
+
+        }
+    }
+}
